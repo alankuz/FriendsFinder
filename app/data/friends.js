0cdc77b41a21389c3dd86fbfb9562b528ffcbf8f
Alan <alankuzmanovic@gmail.com>
2019-10-02 23:41:23 -0600

    added api routes and friends list, and server

---
+++ app/data/friends.js
@@ -0,0 +1,146 @@
+var friendlist =[
+    {
+        name: "Conner Baldwin",
+        photo: "https://cdn.nybooks.com/wp-content/uploads/2016/07/conner-frankie-fix.jpg",
+        scores: [
+            '2',
+            '3',
+            '3',
+            '3',
+            '2',
+            '2',
+            '1',
+            '5',
+            '3',
+            '2'
+        ]
+    },
+    {
+        name: "James Frank",
+        photo: "http://www.gstatic.com/tv/thumb/persons/167024/167024_v9_bb.jpg",
+        scores: [
+            '3',
+            '4',
+            '5',
+            '5',
+            '1',
+            '1',
+            '4',
+            '2',
+            '4',
+            '1'
+        ]
+    },
+    {
+        name: "Scott Mitchell",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest/scale-to-width-down/2000?cb=20170701090332",
+        scores: [
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '2',
+            '2'
+        ]
+    },
+    {
+        name: "Prison Mitchell",
+        photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/96/Prisonmike.png/revision/latest?cb=20100327171549",
+        scores: [
+            '4',
+            '3',
+            '4',
+            '3',
+            '3',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: "Moon Face",
+        photo: "https://i.imgur.com/JEWpRQU.jpg",
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: "Scared Dog",
+        photo: "http://votefinchem.com/wp-content/uploads/2018/06/Coward-590x400.jpg",
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: "cich nage",
+        photo: "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/nicolascage-faceoff-crazy.jpg",
+        scores: [
+            '3',
+            '4',
+            '1',
+            '1',
+            '2',
+            '2',
+            '2',
+            '4',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: "No Eyebrowz",
+        photo: "https://www.indiewire.com/wp-content/uploads/2019/02/Screen-Shot-2019-02-20-at-9.16.53-AM.png?w=780",
+        scores: [
+            '4',
+            '4',
+            '4',
+            '1',
+            '1',
+            '2',
+            '1',
+            '3',
+            '3',
+            '1'
+        ]
+    },
+    {
+        name: "Chancleor Bennett",
+        photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2018%2F11%2Fgettyimages-980492498.jpg&w=400&c=sc&poi=face&q=85",
+        scores: [
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '4',
+            '0',
+            '0',
+            '0',
+            '0'
+        ]
+    }
+]
