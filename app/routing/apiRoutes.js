8f2d063000014184171468eae3fed864d238f3dc
Alan <alankuzmanovic@gmail.com>
2019-10-03 23:57:38 -0600

    added more server files, added routing for html, created barebones html for the survey, built some api scripting. TODO:make the results go into the api and compute best match, then display best match once this is done.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,45 +2,32 @@ var path = require('path');
 
 var friendList = require("../data/friends.js")
 module.exports = function(app) {
-	app.get('/api/friendslist', function(req, res) {
-		res.json(friendslist);
+	app.get('/api/friendlist', function(req, res) {
+		res.json(friendlist);
 	});
-    app.post('/api/friends', function(req, res) {
+    app.post('/api/friendlist', function(req, res) {
 		var userInput = req.body;
 		var userResponses = userInput.scores;
 
-		// Compute best friend match
 		var matchName = '';
 		var matchImage = '';
-		var totalDifference = 10000; // Make the initial value big for comparison
+		var totalDifference = 10000; 
 
-		// Examine all existing friends in the list
 		for (var i = 0; i < friendList.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
 
-			// Compute differenes for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friendList[i].scores[j] - userResponses[j]);
 			}
-			// console.log('diff = ' + diff);
-
-			// If lowest difference, record the friend match
 			if (diff < totalDifference) {
-				// console.log('Closest match found = ' + diff);
-				// console.log('Friend name = ' + friends[i].name);
-				// console.log('Friend image = ' + friends[i].photo);
-
 				totalDifference = diff;
 				matchName = friendList[i].name;
 				matchImage = friendList[i].photo;
 			}
 		}
 
-		// Add new user
 		friendList.push(userInput);
 
-		// Send appropriate response
 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 	});
 };
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = 8080;
+// var PORT = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, './app/public')));
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
